f298d0e2490f64f3258aec61f050665ebb88a306
David Lovegrove <davidlo@unity3d.com>
2026-05-15 10:34:50 +0100

    Update for mac and win.

--- lua/plugins/roslyn.lua
+++ lua/plugins/roslyn.lua
@@ -4,7 +4,20 @@ return {
   cmd = { "Roslyn" },
   opts = {},
   init = function()
+    local exe
+    if vim.fn.has("win32") == 1 then
+      exe = vim.fn.expand("$LOCALAPPDATA/roslyn-ls/content/LanguageServer/win-x64/Microsoft.CodeAnalysis.LanguageServer.exe")
+    else
+      exe = vim.fn.expand("~/.local/share/roslyn-ls/Microsoft.CodeAnalysis.LanguageServer")
+    end
+
     vim.lsp.config("roslyn", {
+      cmd = {
+        exe,
+        "--logLevel=Information",
+        "--extensionLogDirectory=" .. vim.fn.stdpath("data"),
+        "--stdio",
+      },
       capabilities = require('cmp_nvim_lsp').default_capabilities(),
       on_attach = function(_, bufnr)
         local opts = { buffer = bufnr }
