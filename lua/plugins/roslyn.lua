891a1a5efd67dbffb613f25cc5170a47191abd8b
unitydavidlo <davidlo@unity3d.com>
2026-06-02 16:19:19 +0100

    Fixed errors.

--- lua/plugins/roslyn.lua
+++ lua/plugins/roslyn.lua
@@ -13,7 +13,19 @@ return {
         local function tab_drop_list(o)
           local item = o.items[1]
           if not item then return end
-          vim.cmd("tab drop " .. vim.fn.fnameescape(item.filename))
+          local path = vim.fn.fnamemodify(item.filename, ":p")
+          for _, tab in ipairs(vim.api.nvim_list_tabpages()) do
+            for _, win in ipairs(vim.api.nvim_tabpage_list_wins(tab)) do
+              local buf = vim.api.nvim_win_get_buf(win)
+              if vim.api.nvim_buf_get_name(buf) == path then
+                vim.api.nvim_set_current_tabpage(tab)
+                vim.api.nvim_set_current_win(win)
+                vim.api.nvim_win_set_cursor(0, { item.lnum, item.col - 1 })
+                return
+              end
+            end
+          end
+          vim.cmd("tabnew " .. vim.fn.fnameescape(path))
           vim.api.nvim_win_set_cursor(0, { item.lnum, item.col - 1 })
         end
         vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition({ on_list = tab_drop_list }) end, opts)
